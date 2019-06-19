468abdd368c38ff3feb81d33c74d488f2de231ce
scarfacedeb <scarfacedeb@gmail.com>
2019-06-19 20:43:32 +0300

    Replace Plug.Conn.send_file with Plug.Conn.send_resp in Exfile.Router
    Why:
    
    Quickfix for cowboy 2 issue: https://github.com/keichan34/exfile/issues/64

--- lib/exfile/router.ex
+++ lib/exfile/router.ex
@@ -6,13 +6,14 @@ defmodule Exfile.Router do
 
   require Logger
 
-  plug Plug.Parsers, parsers: [:multipart]
-  plug :match
-  plug :dispatch
+  plug(Plug.Parsers, parsers: [:multipart])
+  plug(:match)
+  plug(:dispatch)
 
   defp authenticate(%{path_info: path_info} = conn) do
     [token | rest] = path_info
     request_path = "/" <> Enum.join(rest, "/")
+
     if Exfile.Token.verify_token(request_path, token) do
       conn
     else
@@ -21,8 +22,10 @@ defmodule Exfile.Router do
   end
 
   defp download_allowed?(%{halted: true} = conn, _), do: conn
+
   defp download_allowed?(conn, backend) do
-    backends = Config.allow_downloads_from
+    backends = Config.allow_downloads_from()
+
     if backends == :all || Enum.member?(backends, backend) do
       conn
     else
@@ -31,8 +34,10 @@ defmodule Exfile.Router do
   end
 
   defp upload_allowed?(%{halted: true} = conn, _), do: conn
+
   defp upload_allowed?(conn, backend) do
-    backends = Config.allow_uploads_to
+    backends = Config.allow_uploads_to()
+
     if backends == :all || Enum.member?(backends, backend) do
       conn
     else
@@ -41,12 +46,16 @@ defmodule Exfile.Router do
   end
 
   defp check_or_set_etag(%{halted: true} = conn, _), do: conn
+
   defp check_or_set_etag(conn, token) do
-    remote_etag = case get_req_header(conn, "if-none-match") do
-      [etag | _] -> etag
-      _ -> nil
-    end
+    remote_etag =
+      case get_req_header(conn, "if-none-match") do
+        [etag | _] -> etag
+        _ -> nil
+      end
+
     conn = put_resp_header(conn, "etag", token)
+
     if token == remote_etag do
       send_resp(conn, 304, "") |> halt
     else
@@ -104,21 +113,26 @@ defmodule Exfile.Router do
   end
 
   defp set_file(%{halted: true} = conn, _, _), do: conn
+
   defp set_file(conn, backend, id) do
     file = %Exfile.File{
       id: id,
       backend: Config.get_backend(backend)
     }
+
     assign(conn, :exfile_backend_file, file)
     |> load_file
   end
 
   defp load_file(%{halted: true} = conn), do: conn
+
   defp load_file(%{assigns: %{exfile_backend_file: file}} = conn) do
-    Logger.debug "[exfile] opening file: #{inspect file}"
+    Logger.debug("[exfile] opening file: #{inspect(file)}")
+
     case Exfile.File.open(file) do
       {:ok, local_file} ->
         assign(conn, :exfile_local_file, local_file)
+
       _error ->
         send_resp(conn, 404, "file not found") |> halt
     end
@@ -126,7 +140,8 @@ defmodule Exfile.Router do
 
   defp extract_format(%{path_info: path_info}) do
     filename = List.last(path_info)
-    ext = String.split(filename, ".") |> List.last
+    ext = String.split(filename, ".") |> List.last()
+
     if filename == ext do
       :error
     else
@@ -135,6 +150,7 @@ defmodule Exfile.Router do
   end
 
   defp apply_format_processing(%{halted: true} = conn), do: conn
+
   defp apply_format_processing(conn) do
     case extract_format(conn) do
       {:ok, ext} -> format_whitelisted(conn, ext)
@@ -143,61 +159,73 @@ defmodule Exfile.Router do
   end
 
   defp format_whitelisted(conn, ext) do
-    if Enum.member?(Config.convert_formats, ext),
+    if Enum.member?(Config.convert_formats(), ext),
       do: process_file(conn, "convert", [ext]),
       else: conn
   end
 
   defp process_file(conn, processor, args \\ [])
   defp process_file(%{halted: true} = conn, _, _), do: conn
+
   defp process_file(%{assigns: %{exfile_local_file: file}} = conn, processor, args) do
     opts = []
-    opts = case extract_format(conn) do
-      {:ok, ext} -> Keyword.put(opts, :format, ext)
-      :error -> opts
-    end
+
+    opts =
+      case extract_format(conn) do
+        {:ok, ext} -> Keyword.put(opts, :format, ext)
+        :error -> opts
+      end
+
     case Exfile.ProcessorRegistry.process(processor, file, args, opts) do
       {:ok, processed_file} ->
         assign(conn, :exfile_local_file, processed_file)
+
       {:error, reason} ->
         send_resp(conn, 500, "processing using #{processor} failed with reason #{reason}") |> halt
     end
   end
 
   defp stream_file(%{halted: true} = conn), do: conn
-  defp stream_file(%{assigns: %{exfile_local_file: %LocalFile{path: path}}} = conn) when not is_nil(path) do
+
+  defp stream_file(%{assigns: %{exfile_local_file: %LocalFile{path: path}}} = conn)
+       when not is_nil(path) do
+    io = File.open!(path)
+
+    conn
+    |> assign(:exfile_local_file, %LocalFile{io: io})
+    |> stream_file()
+  end
+
+  defp stream_file(%{assigns: %{exfile_local_file: %LocalFile{io: io}}} = conn)
+       when not is_nil(io) do
+    data = IO.binread(io, :all)
     filename = List.last(conn.path_info)
+
     conn
     |> put_resp_header("content-disposition", "inline; filename=#{filename}")
     |> put_resp_header("expires", expires_header())
     |> put_resp_header("cache-control", "max-age=31540000")
-    |> set_content_type(path)
-    |> send_file(200, path)
-  end
-  defp stream_file(%{assigns: %{exfile_local_file: %LocalFile{io: io}}} = conn) when not is_nil(io) do
-    data = IO.binread(io, :all)
-    file = Exfile.Tempfile.random_file!("send")
-    File.write!(file, data, [:write])
-
-    conn
-    |> assign(:exfile_local_file, %LocalFile{path: file})
-    |> stream_file
+    |> set_content_type(filename)
+    |> send_resp(200, data)
   end
 
-  defp set_content_type(conn, location) do
-    case Exfile.Identify.mime_type(location) do
-      {:ok, mime_type} ->
-        put_resp_header(conn, "content-type", mime_type)
-      :error ->
-        conn
-    end
+  defp set_content_type(conn, filename) do
+    mime_type =
+      case filename do
+        nil -> "application/octet-stream"
+        _ -> MIME.from_path(filename)
+      end
+
+    put_resp_header(conn, "content-type", mime_type)
   end
 
   defp process_uploaded_file(%{halted: true} = conn, _), do: conn
+
   defp process_uploaded_file(conn, backend) do
     file = conn.params["file"]
     process_uploaded_file(conn, backend, file)
   end
+
   defp process_uploaded_file(conn, backend, %Plug.Upload{} = uploaded_file) do
     local_file = %LocalFile{path: uploaded_file.path}
     backend = Config.get_backend(backend)
@@ -207,19 +235,22 @@ defmodule Exfile.Router do
       {:ok, file} ->
         uri = Exfile.File.uri(file)
         send_resp(conn, 200, ~s({"id":"#{file.id}","uri":"#{uri}"}))
+
       {:error, reason} ->
         send_resp(conn, 422, ~s({"error":true,"error_message":"#{reason}"}))
     end
   end
+
   defp process_uploaded_file(conn, _backend, nil) do
     send_resp(conn, 400, "please upload a file") |> halt
   end
 
   defp expires_header() do
-    seconds = :calendar.local_time |> :calendar.datetime_to_gregorian_seconds
-    (seconds + (365 * 24 * 60 * 60))
-    |> :calendar.gregorian_seconds_to_datetime
-    |> :httpd_util.rfc1123_date
+    seconds = :calendar.local_time() |> :calendar.datetime_to_gregorian_seconds()
+
+    (seconds + 365 * 24 * 60 * 60)
+    |> :calendar.gregorian_seconds_to_datetime()
+    |> :httpd_util.rfc1123_date()
     |> to_string
   end
 end
